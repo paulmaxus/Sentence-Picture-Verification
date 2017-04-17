11635b0edbaf8e9ccd5fe527e8039a41d2789f61
paulmaxus <paulmaxus@gmail.com>
2017-04-17 12:16:15 +0200

    add experiment

---
+++ Pilot/instructionConfig.m
@@ -0,0 +1,76 @@
+function [textExp,button] = instructionConfig(const)
+% ----------------------------------------------------------------------
+% Goal of the function :
+% Write text of calibration and general instruction for the experiment.
+% ----------------------------------------------------------------------
+% Input(s) :
+% (none)
+% ----------------------------------------------------------------------
+% Output(s):
+% textExp : struct containing all text of general instructions.
+% button : struct containing all button instructions.
+% ----------------------------------------------------------------------
+
+%% Pause:
+pause_l1 = 'Pausa.';
+pause_b1 = '-------------- Premi la barra spaziatrice per continuare l''esperimento --------------';
+
+textExp.pause = {pause_l1};
+button.pause = {pause_b1};
+
+%% Pause after practice:
+pracover_l1 = 'Pronto?';
+pracover_b1 = '-------------- Premi la barra spaziatrice per cominciare l''esperimento --------------';
+
+textExp.pracover = {pracover_l1};
+button.pracover = {pracover_b1};
+
+%% Pause after block (change to difficult):
+blockover0_l1 = 'La prima parte � finita.';
+blockover0_l2 = 'Ci sar� un''altro esercizio prima della seconda parte.';
+blockover0_b1 = '-------------- Premi la barra spaziatrice per cominciare l''esercizio --------------';
+
+textExp.blockover0 = {blockover0_l1;blockover0_l2};
+button.blockover0 = {blockover0_b1};
+
+%% Pause after block (change to easy):
+blockover1_l1 = 'La prima parte � finita.';
+blockover1_l2 = 'Ci sar� un''altro esercizio prima della seconda parte.';
+blockover1_b1 = '-------------- Premi la barra spaziatrice per cominciare l''esercizio --------------';
+
+textExp.blockover1 = {blockover1_l1;blockover1_l2};
+button.blockover1 = {blockover1_b1};
+
+
+%% End :
+end_l1 = 'Fine.';
+end_b1 = '-------------- Informa l''esperimentatore --------------';
+
+textExp.end = {end_l1};
+button.end =  {end_b1};
+
+%% Main instruction :
+
+instruction_l1  =  'Il tuo compito sar� quello di leggere delle frasi.';
+instruction_l2  =  'Successivamente ti verr� presentata un''immagine che potr� corrispondere';
+instruction_l3  =  'o meno a quanto letto precedentemente.';
+instruction_l4  =  '';
+if const.sjct_blockseq == 0
+    % up is matching
+    instruction_l5  =  'Dovrai premere lo FRECCIA SU se la frase e l''immagine corrispondono,';
+    instruction_l6  =  'o lo FRECCIA GIU se NON corrispondono.';
+else
+    % down is matching
+    instruction_l5  =  'Dovrai premere lo FRECCIA GIU se la frase e l''immagine corrispondono,';
+    instruction_l6  =  'o lo FRECCIA SU se NON corrispondono.';
+end
+instruction_l7  =  '';
+instruction_l8  =  'Le figure potranno essere: cerchi, triangoli o rettangoli.';
+instruction_l9  =  'Saranno colorate di bianco o di nero.';
+
+instruction_b1 = '-------------- Premi la barra spaziatrice per cominciare l''esercizio --------------';
+
+textExp.instruction1= {instruction_l1;instruction_l2;instruction_l3;instruction_l4;instruction_l5; instruction_l6; instruction_l7; instruction_l8; instruction_l9};
+button.instruction1 =  {instruction_b1};
+
+end
---
+++ Pilot/launcher.m
@@ -0,0 +1,43 @@
+%-------------------------- launches the experiment ----------------------------
+
+%TODO this is for debug
+Screen('Preference', 'SkipSyncTests', 1);
+Screen('Preference', 'VisualDebugLevel', 0);
+Screen('Preference', 'SuppressAllWarnings', 1);
+
+% Name and mode
+%const.expName = 'SPV16';
+const.expStart = 1; % real or debug (=0) mode
+
+% Subject configuration :
+if const.expStart
+    %const.sjct = input(sprintf('\n\tInitials: '),'s');
+    const.sjct_age = input(sprintf('\n\tAge: '));
+    const.sjct_gender = input(sprintf('\n\tGender (m = 0 or f = 1): '));
+    const.sjct_number = input(sprintf('\n\tNumber: '));
+else
+    %const.sjct = 'Test';
+    const.sjct_age = 16;
+    const.sjct_gender = 0;
+    const.sjct_number = 0; % delay and key
+end
+
+% block (delay) sequence and button order
+const.sjct_blockseq = mod(const.sjct_number,2); % mod = 0 for even, 1 for odd
+
+% specify data file
+const.expRes_fileCsv = sprintf('%u.csv',const.sjct_number);
+if const.expStart
+    cd ('Data');
+    if exist(const.expRes_fileCsv, 'file') == 2
+        cd('..');
+        error('Subject code already exists');
+    end
+    cd('..');
+end
+
+%% Main experimental code
+
+ListenChar(2); % deactivate input
+main(const);
+%ListenChar(1); % activate input
